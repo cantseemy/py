3ca7a6e0835836b83dfea4b9d607870d5e9d29f5
cantseemy <32155433+cantseemy@users.noreply.github.com>
2017-09-21 15:23:03 +0300

    Add files via upload

---
+++ Project_2_2.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python3
+
+"""
+Сметана Тарас 
+
+"""
+sname, fname,tname = input('Прізвище, Ім\'я, По-батькові : ').split(sep = ' ')
+print('Привіт ' + sname + ' ' + fname + ' ' + tname)
