f54c6371157e0c456e1f23836af619ded72dbd33
cantseemy <32155433+cantseemy@users.noreply.github.com>
2017-09-28 17:56:59 +0300

    Update Project_5_5.py

--- Project_5_5.py
+++ Project_5_5.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python3
 
 a = int(input("Введіть число : "))
-
-if a % 2 == 0:
-	print("Число складене")
-else:
-	print("Число просте")
+i=0
+s=0
+while i<=a:
+	i=i+1
+	if a%i==0:
+		s=s+1
+if s <= 2:
+	print("Число просте ")
+if s > 2:
+	print("Число складене ")
+	
